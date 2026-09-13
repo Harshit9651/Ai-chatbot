53373b14aef96e1c6db7ded407661e67c35a008f
harshit9651 <programming966051@gmail.com>
2026-09-13 14:06:09 +0530

    do some changes

--- frontend/src/api/chatApi.js
+++ frontend/src/api/chatApi.js
@@ -1,7 +1,4 @@
-const API_BASE_URL =
-  "http://127.0.0.1:8000/api/v1/chat";
-
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 // Get complete chat history
 export const getChatHistory = async (threadId) => {
   const response = await fetch(
